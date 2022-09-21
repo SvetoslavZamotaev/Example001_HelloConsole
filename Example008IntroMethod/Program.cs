616a75b3fb7cc5aaae0120a55e8b6022e1d473d2
SvetoslavZamotaev <Tomasangelo3333@mail.ru>
2022-09-21 10:22:21 +0300

    008 папка

---
+++ Example008IntroMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a = 4;
+int b = 6;
+int c = 8;
+Console.WriteLine(Max(a, b, c));
